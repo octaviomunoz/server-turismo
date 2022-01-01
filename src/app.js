8430b6bebd372218598aec1e7860205bd2222983
octaviomunoz <ostavio09@gmail.com>
2021-12-31 23:07:17 -0300

    config initial

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
-const express = require('express')
+import express from 'express'
+import morgan from 'morgan'
+import pkg from '../package.json'
+
 const app = express()
-const port = 3001
+
+app.set('pkg', pkg)
+
+app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
-	res.send('Hello World!')
+  res.json({
+    name: app.get('pkg').name,
+    author: app.get('pkg').author,
+    description: app.get('pkg').description,
+    version: app.get('pkg').version,
+  })
 })
 
-app.listen(port, () => {
-	console.log(`Ejemplo app en http://localhost:${port}`)
-})
+export default app
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import app from './app'
+
+app.listen(4000)
+console.log('SERVER LIST  ON PORT', 4000)
