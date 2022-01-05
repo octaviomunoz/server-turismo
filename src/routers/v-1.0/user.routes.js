8500464afda8965e1d2903bb217d56bfd6c550b5
octaviomunoz <ostavio09@gmail.com>
2022-01-05 20:09:11 -0300

    delete file not used

--- src/routers/v-1.0/user.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import * as authController from '../../controller/auth.controller'
-
-const router = Router()
-
-router.post('/login', authController.login)
-
-export default router
