56b9d843124ebd545408a0dd2d62192cef21576e
octaviomunoz <ostavio09@gmail.com>
2022-01-06 11:53:52 -0300

    selecting the repositoryof database used

---
+++ src/infrastructure/config/service-locator.js
@@ -0,0 +1,18 @@
+import {CONST_SUPPORTED_DATABASE} from './constants'
+import {database} from './environment'
+import UserSerializer from '../../interface/serializers/UserSerializer'
+import UserRepositoryMongo from '../repositories/mongoose/UserRepositoryMongo'
+
+
+function buildBeans() {
+    const beans = {
+        userSerializer = UserSerializer
+    }
+
+    if (database.dialect === CONST_SUPPORTED_DATABASE.MONGO){
+        beans.userRepository = new UserRepositoryMongo()
+    }
+}
+
+export default buildBeans
+
