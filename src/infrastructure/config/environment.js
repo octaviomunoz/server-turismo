0bf6a1e529868ffbbd975bdbff8271bd6e149439
octaviomunoz <ostavio09@gmail.com>
2022-01-06 13:49:57 -0300

    import dotenv

--- src/infrastructure/config/environment.js
+++ src/infrastructure/config/environment.js
@@ -1,4 +1,8 @@
+import dotenv from 'dotenv'
+
 import { CONST_SUPPORTED_DATABASE } from './constants'
+
+dotenv.config()
 
 export const database = {
   dialect: process.env.DATABASE_DIALECT || CONST_SUPPORTED_DATABASE.MONGO,
